e2d68c1b51bca23e28fe5ee49ed5785abdd8f2bc
debjohnson33 <debjohnson_11@yahoo.com>
2019-08-14 15:09:53 -0500

    Adds blades action file/fetchBlades

---
+++ src/actions/blades.js
@@ -0,0 +1,33 @@
+const sqlite3 = require('sqlite3');
+
+const db = new sqlite3.Database('dev.sqlite3', (err) => {
+    if (err) {
+        console.log(err.message);
+    } else {
+        db.run('CREATE TABLE IF NOT EXISTS Blades (id INTEGER PRIMARY KEY, stensNumber TEXT, quantity INTEGER, manufacturerNumber TEXT)')
+    }
+    console.log('Connected to the development database');
+});
+
+export const bladesFetchSuccess = blades => {
+	return {
+		type: 'BLADES_FETCH_DATA_SUCCESS',
+		blades
+	}
+}
+
+export const fetchBlades = () => {
+	return dispatch => {
+		return db.serialize(() => {
+            db.each("SELECT * FROM Blades", (err, row) => {
+                if (err) {
+                    console.log(err.message);
+                }
+            })
+        }).then(response => response.json())
+        .then(blades => {
+            dispatch(bladesFetchSuccess(blades));
+        })		
+		.catch(error => console.log(error));
+	};
+};
