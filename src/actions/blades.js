0a085bd72e3bbc987488ccc52d3d901716c8a7ca
debjohnson33 <debjohnson_11@yahoo.com>
2019-08-14 17:00:32 -0500

    Edits to App/blades action

--- src/actions/blades.js
+++ src/actions/blades.js
@@ -1,13 +1,13 @@
-const sqlite3 = require('sqlite3');
+//const sqlite3 = require('sqlite3');
 
-const db = new sqlite3.Database('dev.sqlite3', (err) => {
-    if (err) {
-        console.log(err.message);
-    } else {
-        db.run('CREATE TABLE IF NOT EXISTS Blades (id INTEGER PRIMARY KEY, stensNumber TEXT, quantity INTEGER, manufacturerNumber TEXT)')
-    }
-    console.log('Connected to the development database');
-});
+// const db = new sqlite3.Database('dev.sqlite3', (err) => {
+//     if (err) {
+//         console.log(err.message);
+//     } else {
+//         db.run('CREATE TABLE IF NOT EXISTS Blades (id INTEGER PRIMARY KEY, stensNumber TEXT, quantity INTEGER, manufacturerNumber TEXT)')
+//     }
+//     console.log('Connected to the development database');
+// });
 
 export const bladesFetchSuccess = blades => {
 	return {
@@ -18,16 +18,16 @@ export const bladesFetchSuccess = blades => {
 
 export const fetchBlades = () => {
 	return dispatch => {
-		return db.serialize(() => {
-            db.each("SELECT * FROM Blades", (err, row) => {
-                if (err) {
-                    console.log(err.message);
-                }
-            })
-        }).then(response => response.json())
-        .then(blades => {
-            dispatch(bladesFetchSuccess(blades));
-        })		
-		.catch(error => console.log(error));
+		// return db.serialize(() => {
+        //     db.each("SELECT * FROM Blades", (err, row) => {
+        //         if (err) {
+        //             console.log(err.message);
+        //         }
+        //     })
+        // }).then(response => response.json())
+        // .then(blades => {
+        //     dispatch(bladesFetchSuccess(blades));
+        // })		
+		// .catch(error => console.log(error));
 	};
 };
